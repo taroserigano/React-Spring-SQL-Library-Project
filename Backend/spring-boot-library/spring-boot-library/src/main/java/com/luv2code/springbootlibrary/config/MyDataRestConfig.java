cfdeeee76b24610c0b873e32cbc4dc1de99001d1
Taro Serigano <taroserigano@gmail.com>
2022-12-14 14:46:01 -0600

    Update MyDataRestConfig.java

--- Backend/spring-boot-library/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/config/MyDataRestConfig.java
+++ Backend/spring-boot-library/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/config/MyDataRestConfig.java
@@ -12,38 +12,43 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
 
-    private String theAllowedOrigins = "http://localhost:3000";
+    private String theAllowedOrigins = "http://localhost:3000";  // this IP add will be allowed to access 
 
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config,
                                                      CorsRegistry cors) {
-        HttpMethod[] theUnsupportedActions = {
+        // HTTP reqs to be blocked 
+        HttpMethod[] theUnsupportedActions = {          
                 HttpMethod.POST,
                 HttpMethod.PATCH,
                 HttpMethod.DELETE,
                 HttpMethod.PUT};
-
+        
+        // specify primary IDs of these classes 
         config.exposeIdsFor(Book.class);
         config.exposeIdsFor(Review.class);
         config.exposeIdsFor(Message.class);
-
+        
+        // disable them 
         disableHttpMethods(Book.class, config, theUnsupportedActions);
         disableHttpMethods(Review.class, config, theUnsupportedActions);
         disableHttpMethods(Message.class, config, theUnsupportedActions);
 
         /* Configure CORS Mapping */
         cors.addMapping(config.getBasePath() + "/**")
-                .allowedOrigins(theAllowedOrigins);
+                .allowedOrigins(theAllowedOrigins); // <- allow this IP add to access 
     }
 
-    private void disableHttpMethods(Class theClass,
-                                    RepositoryRestConfiguration config,
-                                    HttpMethod[] theUnsupportedActions) {
-        config.getExposureConfiguration()
-                .forDomainType(theClass)
-                .withItemExposure((metdata, httpMethods) ->
+    private void disableHttpMethods(Class theClass,  // class to disable 
+                                    RepositoryRestConfiguration config,  // set the IDs 
+                                    HttpMethod[] theUnsupportedActions)   // blocking HTP reqs 
+    {
+        
+        config.getExposureConfiguration()        
+                .forDomainType(theClass)        // specify class 
+                .withItemExposure((metdata, httpMethods) ->        // single item 
                         httpMethods.disable(theUnsupportedActions))
-                .withCollectionExposure((metdata, httpMethods) ->
+                .withCollectionExposure((metdata, httpMethods) ->  // collection items 
                         httpMethods.disable(theUnsupportedActions));
     }
 }
